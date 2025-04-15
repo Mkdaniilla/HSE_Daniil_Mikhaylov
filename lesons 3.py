ff749196ec51b4cbbd3cd37ec3aef905c6c82700
dany test <mkdaniil@yandex.ru>
2025-04-15 15:06:12 +0300

    Обновил lesons 3.py и другие файлы

--- lesons 3.py
+++ lesons 3.py
@@ -1,3 +1,31 @@
 import time
+from lesson_3_data import courts
 a = 0
-while True
+while True:
+    a += 1
+    if a % 2 != 0:
+        continue
+    elif a > 20:
+        break
+    else:
+        print('a =', a)
+
+print('end of while cycle')
+
+connected = False
+
+a, b = 10, 15
+
+names = []
+for court in courts:
+    names.append(court['court_name'])
+
+names = [court['court_name'] for court in courts]
+
+
+court_mapping = {court['court_code']: court['court_name'] for court in courts}
+court_set = {court['court_name'] for court in courts}
+
+print(court_mapping)
+
+print('end of for cycle')
